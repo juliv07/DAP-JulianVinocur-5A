cae79b66c351367b7ac9f45b4fe43cd118041db6
juliv07 <julivinocur@gmail.com>
2024-05-28 19:26:45 -0300

    Update home_screen.dart
    arrgelé detalles

--- Clase 3/login_con_otra_pantalla/lib/presentarions/home_screen.dart
+++ Clase 3/login_con_otra_pantalla/lib/presentarions/home_screen.dart
@@ -8,7 +8,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     List albums = [
-      '11','19','1989','21','25','27','30','¿A Dónde Vamos?','After Hours' 'Balas Perdidas',
+      '11','19','1989','21','25','27','30','¿A Dónde Vamos?','After Hours','Balas Perdidas',
       'Beauty Behind The Madness','Cada Letra Que Escribo','Canciones del Huracan + Wonderful Noches',
       'CARAVANA','Chemical','Cicatrices','Columbia','Cositas','The Days / Nights','De Bichos Y Flores',
       'Deseando Mis Pasos','Destinología','Doo-Wops & Hooligans','El Llamado del Sol','El Otro','THE E.N.D. (THE ENERGY NEVER DIES)',
@@ -25,8 +25,8 @@ class HomeScreen extends StatelessWidget {
       'The Weeknd','Morat','The Weeknd','Koino Yokan','Tan Bionica','WOS','Post Malone','La ultima carta',
       'Quevedo','Benjamín Amadeo','Avicii','La Vela Puerca','Koino Yokan','Tan Bionica','Bruno Mars',
       'Koino Yokan','Chano','Black Eyed Peas','FLETCHER','Ciro y los Persas','Taylor Swift','Taylor Swift',
-      'Taylor Swift','One Direction','Coldplay','Árbol','Tan Bionica','Sam Smith','Queen','Airbag',
-      'Julieta Venegas','Koino Yokan','Koino Yokan','Taylor Swift','Florence + The Machine','Koino Yokan',
+      'Taylor Swift','One Direction','Coldplay','Árbol','Tan Bionica','Sam Smith','Queen','Conociendo Rusia',
+      'Airbag','Julieta Venegas','Koino Yokan','Koino Yokan','Taylor Swift','Florence + The Machine','Koino Yokan',
       'Coldplay','Queen','Tan Bionica','WOS','Coldplay','Koino Yokan','El Cuarteto De Nos','Daddy Yankee',
       'Katy Perry','Benjamín Amadeo','El Cuarteto De Nos','Taylor Swift','Taylor Swift','Callejeros','fun.',
       'Taylor Swift','The Weeknd','Avicii','WALK THE MOON','Rihanna','Katy Perry','Sia','Avicii','Rihanna',
@@ -44,7 +44,7 @@ class HomeScreen extends StatelessWidget {
         itemCount: albums.length,
         itemBuilder: (context, index) {
           return Card(
-            color: Color.fromARGB(255, 148, 198, 207),
+            color: Color.fromARGB(255, 194, 245, 255),
             child: ListTile(
               title: Text(albums[index]),
               subtitle: Text(artists[index]),
